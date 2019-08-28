87ec4b4b7ccd6ab40a63928f99829849ed48e059
coliveira <Cleysonb.oliveira@gmail.com>
2019-08-28 13:04:39 -0300

    Angular project structure created

---
+++ frontend/src/app/blueprint/blueprint.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blueprint',
+  templateUrl: './blueprint.component.html',
+  styleUrls: ['./blueprint.component.css']
+})
+export class BlueprintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('IT\'S HERE');
+  }
+
+
+  
+}

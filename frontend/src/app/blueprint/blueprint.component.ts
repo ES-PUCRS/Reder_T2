f676df20118766d94260cf45b5b42ba5c2ae89be
coliveira <Cleysonb.oliveira@gmail.com>
2019-08-30 13:08:34 -0300

    right click drop down save before test

--- frontend/src/app/blueprint/blueprint.component.ts
+++ frontend/src/app/blueprint/blueprint.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { fromEvent, Subscription } from 'rxjs';
+import { take, filter } from 'rxjs/operators';
 import { state } from '@angular/animations';
-//ngStyle
 
 @Component({
   selector: 'app-blueprint',
@@ -31,10 +34,91 @@ export class BlueprintComponent implements OnInit {
     this.states.push(a);
     console.log(this.states);
   }
+
 }
 
-interface state{
+export interface state{
   id: string,
   final: boolean,
   transition: {key: string, state: state}
 }
+
+
+/*
+
+
+import { Component, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
+import faker from 'faker';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { fromEvent, Subscription } from 'rxjs';
+import { take, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  users = Array.from({ length: 10 }, () => ({
+    name: faker.name.findName()
+  }));
+  sub: Subscription;
+
+  @ViewChild('userMenu') userMenu: TemplateRef<any>;
+
+  overlayRef: OverlayRef | null;
+
+  constructor(
+    public overlay: Overlay,
+    public viewContainerRef: ViewContainerRef) {
+  }
+
+  open({ x, y }: MouseEvent, user) {
+    this.close();
+    const positionStrategy = this.overlay.position()
+      .flexibleConnectedTo({ x, y })
+      .withPositions([
+        {
+          originX: 'end',
+          originY: 'bottom',
+          overlayX: 'end',
+          overlayY: 'top',
+        }
+      ]);
+
+    this.overlayRef = this.overlay.create({
+      positionStrategy,
+      scrollStrategy: this.overlay.scrollStrategies.close()
+    });
+
+    this.overlayRef.attach(new TemplatePortal(this.userMenu, this.viewContainerRef, {
+      $implicit: user
+    }));
+
+    this.sub = fromEvent<MouseEvent>(document, 'click')
+      .pipe(
+        filter(event => {
+          const clickTarget = event.target as HTMLElement;
+          return !!this.overlayRef && !this.overlayRef.overlayElement.contains(clickTarget);
+
+        }),
+        take(1)
+      ).subscribe(() => this.close())
+
+  }
+
+  delete(user) {
+    // delete user
+    this.close();
+  }
+
+  close() {
+    this.sub && this.sub.unsubscribe();
+    if (this.overlayRef) {
+      this.overlayRef.dispose();
+      this.overlayRef = null;
+    }
+  }
+}
+*/

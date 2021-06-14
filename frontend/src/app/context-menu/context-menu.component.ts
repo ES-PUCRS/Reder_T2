0e91b3c206b45f0249fa3e2ec62de80669cf911d
johnnyloreano <laureano.pedrojoao@gmail.com>
2021-06-14 19:22:08 -0300

    Funcoes no COntext-Menu & passando router para o context-menu

--- frontend/src/app/context-menu/context-menu.component.ts
+++ frontend/src/app/context-menu/context-menu.component.ts
@@ -1,5 +1,6 @@
 import { fn } from '@angular/compiler/src/output/output_ast';
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-context-menu',
@@ -7,31 +8,48 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./context-menu.component.scss']
 })
 export class ContextMenuComponent implements OnInit {
-  menuOptions: Array<String> = ["option 1", "option 2", "option 3"];
-  menuMethods: Array<() => void>;
-
+  menuOptions: Array<String> = ["Connect Router", "Delete Router", "Configure Module", "New Module"];
+  menuMethods: Array<any>;
 
   @Input() x = 0;
   @Input() y = 0;
+  @Input() router: any;
 
   constructor() {
 
-    this.menuMethods = [this.testA, this.testB, this.testC];
+    this.menuMethods = [
+      () => { this.connectRouter(this.x) },
+      () => { this.deleteRouter(this.y) },
+      () => { this.configureModule(this.router) },
+      () => { this.newModule(this.router) }
+    ];
 
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  testA = function(): void {
-    alert("A");
+  connectRouter = function (test: any): void {
+    alert("Connect Router");
+
+    alert(test);
   }
 
-  testB() {
-    alert("B");
+  deleteRouter(test: any) {
+    alert("Delete Router");
+
+    alert(test);
   }
 
-  testC() {
-    alert("C");
+  configureModule(test: any) {
+    alert("Configure Module");
+
+    alert(test);
+  }
+
+  newModule(test: any) {
+    alert("New Module");
+
+    alert(test);
   }
 }
